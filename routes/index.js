d8f93f5155b2bbbc253f6bcd6e85e1a17e2368e8
Dylan <dylanliner@gmail.com>
2019-12-15 15:20:53 +0100

    feat: working api call

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var request = require("request");
+var request = require("request")
+, JSONStream = require('JSONStream')
+, es = require('event-stream')
+
 var dotenv = require('dotenv');
+var fs = require('fs');
+
 
 dotenv.config();
 
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-  var options = {
+ 
+  var tokenRequestOptions = {
     method: 'POST',
     url: process.env.POLE_EMPLOI_URL_TOKEN,
-    headers: {'content-type': 'application/x-www-form-urlencoded'},
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
     form: {
       grant_type: 'client_credentials',
       client_id: process.env.POLE_EMPLOI_CLIENT_ID,
@@ -19,13 +26,23 @@ router.get('/', function (req, res, next) {
       scope: process.env.POLE_EMPLOI_SCOPE
     }
   };
-  
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-  
-    console.log(body);
-  });
 
+  var stream = JSONStream.parse(['resultats',true,'lieuTravail',{emitKey: true}]);
+  request(tokenRequestOptions, function (error, response, body) {
+    if (error) throw new Error(error);
+    var token = JSON.parse(body).access_token;
+
+    request.get('https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search', {
+    'auth': {
+      'bearer': token
+    }
+  }).pipe(stream).pipe(es.mapSync(function (data) {
+    console.error(data)
+    return data
+  }))
+  })
+ 
 });
+
 
 module.exports = router;
